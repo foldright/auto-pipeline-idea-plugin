81978c3461b40b734273c5b0bb8eab354aefd27b
zavakid <zava.kid@gmail.com>
2022-11-13 23:08:03 +0800

    add icon

---
+++ src/main/kotlin/com/foldright/autopipeline/autopipelineideaplugin/Icons.kt
@@ -0,0 +1,10 @@
+package com.foldright.autopipeline.autopipelineideaplugin
+
+import com.intellij.openapi.util.IconLoader
+
+object Icons {
+
+    val MARKER = IconLoader.getIcon("/icons/auto-pipeline-icon.svg", Icons.javaClass)
+
+
+}

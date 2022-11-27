dc286622ce6822d17441357fb8a00bd8794e2e88
zavakid <zava.kid@gmail.com>
2022-11-27 19:33:27 +0800

    chore(gradle): support since intellij idea 2022.1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ dependencies {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2021.3.3")
+    version.set("2022.1.4")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf("com.intellij.java"))
